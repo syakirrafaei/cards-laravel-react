b033d56e89a23c974f4a46b513e53dfcc9a3a2e3
syakirrafaei <syakir.rafaei@gmail.com>
2021-01-07 18:57:45 +0800

    Change api baseURL.

--- front-end/src/api.js
+++ front-end/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    withCredentials: false,
-    baseURL: `http://127.0.0.1:8000`,
+  withCredentials: false,
+  baseURL: `https://card-api.syakir.dev`,
 });
